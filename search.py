9cfa40985a5b52b5ed9b19c49d3edb15d3f7b789
Wilson <79236612+wilsonchang17@users.noreply.github.com>
2021-09-27 16:36:54 +0800

    Update search.py

--- search.py
+++ search.py
@@ -10,8 +10,16 @@ from bs4 import BeautifulSoup
 
 def find(arr):
     for i in range(0,len(arr)):
-        if (arr[i]=="例" and  arr[i+1]=="本" and arr[i+2]=="土"):
-
+        if arr[len(arr)-2] == "病" and arr[len(arr)-1] == "例":#處理均為境外移入 新版
+            j = 0
+            while(1):
+                if(arr[j]=='9' or arr[j]=='8' or arr[j]=='7' or arr[j]=='6' or arr[j]=='5' or arr[j]=='4' or arr[j]=='3' or arr[j]=='2' or arr[j]=='1' or arr[j]=='0'):
+                    print(arr[j])
+                    num5.append(arr[j])
+                if arr[j] == "C":
+                    return #因為不同日期用這個方法會list out of range，也因為這邊是處理只有境外移入所以可以直接結束這find()
+                j += 1
+        if arr[i]=="例" and  arr[i+1]=="本" and arr[i+2]=="土":
             j=i-1
             while(1):
                 if(arr[j]=='9' or arr[j]=='8' or arr[j]=='7' or arr[j]=='6' or arr[j]=='5' or arr[j]=='4' or arr[j]=='3' or arr[j]=='2' or arr[j]=='1' or arr[j]=='0'):
@@ -35,15 +43,24 @@ def find(arr):
                 else:
                     break
                 j = j-1
+        if arr[i]=="均" and  arr[i+1]=="為": #處理均為境外移入
+            j = 0
+            while(1):
+                if(arr[j]=='9' or arr[j]=='8' or arr[j]=='7' or arr[j]=='6' or arr[j]=='5' or arr[j]=='4' or arr[j]=='3' or arr[j]=='2' or arr[j]=='1' or arr[j]=='0'):
+                    num4.append(arr[j])
+                if arr[j] == "C":
+                    break
+                j += 1
+
 
 
 time_ = input("請輸入時間")
-print("查詢日期為:  ",time_)
+print("查詢日期為: ",time_)
 time_ = time_.replace("/",".")
 
 option = webdriver.ChromeOptions()
 option.add_argument("headless")
-browser = webdriver.Chrome("D:\google download\chromedriver_win32 (1)\chromedriver.exe", options=option)# chromedriver location in ur computer
+browser = webdriver.Chrome("D:\google download\chromedriver_win32 (1)\chromedriver.exe", options=option)
 browser.get("https://www.cdc.gov.tw/Bulletin/List/MmgtpeidAR5Ooai4-fgHzQ")
 
 elem = browser.find_element_by_xpath('//*[@id="startTime"]')
@@ -62,10 +79,11 @@ browser.find_element_by_xpath('//*[@id="form"]/div/div[3]/button').click()
 sp = BeautifulSoup(browser.page_source, 'lxml')
 data = sp.find_all("p",class_ = "JQdotdotdot")
 arr = ''
-num1 = []
-num2 = []
-num3 = []
-
+num1 = [] #本土
+num2 = [] #境外
+num3 = [] #本土特殊情況
+num4 = [] #均為境外移入
+num5 = [] #均為境外移入 新版
 
 for i in data:
     if i.text[0] == '新' and i.text[1] == '增':
@@ -77,6 +95,8 @@ for i in data:
 #print(num1)
 #print(num2)
 #print(num3)
+#print(num4)
+#print(num5)
 if num3 == []:
     print("本土案例為: ",end = "")
     if num1 == []:
@@ -92,7 +112,14 @@ else:
 print()
 print("境外移入為: ",end = "")
 if num2 == []:
-    print("0")
+    if num4 != []:
+        for i in range(len(num4)):
+            print(num4[i],end="")
+    elif num5 != []:
+        for i in range(len(num5)):
+            print(num5[i],end="")
+    else:
+        print("0")
 else:
     for i in range(len(num2)-1,-1,-1):
         print(num2[i],end="")
